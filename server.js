6bd46c93f90a4765bd0b040840e223537ff52c0e
maneeshanif <manees7865@gmail.com>
2024-10-13 04:47:24 +0500

    "🤔"

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ io.on('connection', (socket) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
